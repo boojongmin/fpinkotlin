64336b62f722081a5824e0c5df5d301154c9839b
부종민 <boojongmin@gmail.com>
2023-10-01 01:34:54 +0900

    exercise 3.14 (14)

--- src/test/kotlin/chapter3/exercises/ex11/listing.kt
+++ src/test/kotlin/chapter3/exercises/ex11/listing.kt
@@ -1,20 +1,25 @@
 package chapter3.exercises.ex11
 
+import chapter3.Cons
 import chapter3.List
+import chapter3.Nil
+import chapter3.foldLeft
+import chapter3.foldRight
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
 import utils.SOLUTION_HERE
 
 // tag::init[]
 fun <A> reverse(xs: List<A>): List<A> =
+    foldLeft(xs, List.empty()) { a, b -> Cons(b, a) }
+// foldRight(xs, List.empty()) { a, b -> Cons(a, b) }
 
-    SOLUTION_HERE()
 // end::init[]
 
 //TODO: Enable tests by removing `!` prefix
 class Exercise11 : WordSpec({
     "list reverse" should {
-        "!reverse list elements" {
+        "reverse list elements" {
             reverse(List.of(1, 2, 3, 4, 5)) shouldBe
                 List.of(5, 4, 3, 2, 1)
         }
--- src/test/kotlin/chapter3/exercises/ex14/listing.kt
+++ src/test/kotlin/chapter3/exercises/ex14/listing.kt
@@ -1,24 +1,28 @@
 package chapter3.exercises.ex14
 
+import chapter3.Cons
 import chapter3.List
+import chapter3.List.Companion.empty
+import chapter3.Nil
+import chapter3.append
+import chapter3.foldRight
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
 import utils.SOLUTION_HERE
 
 // tag::init[]
 fun <A> concat(lla: List<List<A>>): List<A> =
-
-    SOLUTION_HERE()
+    foldRight(lla, empty(), {a1, b1 -> foldRight(a1, b1, {a2, b2 -> Cons(a2, b2)}) })
 
 fun <A> concat2(lla: List<List<A>>): List<A> =
+    foldRight(lla, empty(), {a1: List<A>, b1: List<A> -> append(a1, b1)})
 
-    SOLUTION_HERE()
 // end::init[]
 
 //TODO: Enable tests by removing `!` prefix
 class Exercise14 : WordSpec({
     "list concat" should {
-        "!concatenate a list of lists into a single list" {
+        "concatenate a list of lists into a single list" {
             concat(
                 List.of(
                     List.of(1, 2, 3),
